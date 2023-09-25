fbf159cf7c75ecaf3dea73813009d6c3a161f368
EItan Barylko <eitan@barylko.com>
2023-09-24 23:42:39 -0700

    Added welcome message to home page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-          <div className="is-flex wrapper is-flex-grow-1 has-background-danger-light">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hi there
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="container wrapper is-justify-content-center has-background-danger-light">
+          <div className="is-flex has-text-centered is-justify-content-center title is-size-1" >
+          Welcome to WeatherMe, your friendly weather application
+      </div>
     </div>
   );
 }
