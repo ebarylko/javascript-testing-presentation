7ff85da653056acc9b1d5a7dcea5ce737ba9de87
Eitan Barylko <62489101+ebarylko@users.noreply.github.com>
2023-11-12 09:29:44 -0800

    Incorporate weather api (#8)
    * Added next week's weather to the home page

---
+++ src/app/defaultWeather.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const R = require('ramda');
+const nextWeekTemp = (weather) => R.take(7, weather);
+const toCelsius = (temp) => (temp - 32) * 5 / 9;
+const toSingleDecimal = (temp) => Math.round(temp * 10) / 10;
+const toCelsiusSingleDecimal = R.pipe(toCelsius, toSingleDecimal);
+
+const weatherUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/vancouver"
+
+export function weatherData() {
+    return fetch(
+       `${weatherUrl}?unitGroup=us&key=${process.env.NEXT_PUBLIC_API_KEY}&contentType=json`,
+        {method: 'GET'}
+    ).then(response => {
+        if (!response.ok) {
+            console.log("Error", response)
+            throw response; //check the http response code and if isn't ok then throw the response as an error
+        }
+
+        return response.json()
+    }).then(data => nextWeekTemp(data.days).map(day => toCelsiusSingleDecimal(day.temp)))
+}
+
+export function WeeklyWeather(weather) {
+    console.log(weather.weather)
+    return weather.weather.map((temp, key) => (
+            (<div key={key} className="card">
+                    <div className="card-content">
+                        <div className="content">
+                            {temp} &deg;C
+                        </div>
+                    </div>
+                </div>)
+        )
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,35 @@
+"use client";
 import Image from 'next/image'
 import styles from './page.module.css'
+import * as wd from './defaultWeather.js'
+import {useEffect, useState} from "react";
 
 export default function Home() {
-  return (
+    const [loading, setLoading] = useState(true);
+    const [temperatures, setTemperatures] = useState([]);
+    useEffect(() => {
+        wd.weatherData().then(data => {
+            setLoading(false);
+            console.log(data);
+            setTemperatures(data);
+        });
+    }, []);
+  return(
     <main className="has-background-info">
         <section className="hero is-fullheight is-link">
             <div className="hero-body">
                 <div data-testid= "welcome-message" className="container has-text-centered title">
                         Welcome to Weatherme, your friendly neighbourhood weather app.
                 </div>
-
             </div>
+            {loading && <div> <p>Loading...</p> </div> }
+            {!loading && <div className="container has-text-centered title">
+                <div className={styles.weather}>
+                    <wd.WeeklyWeather weather={temperatures}/>
+                    {/*{temperatures.map((temp, index) => (<li key={index}>{temp}</li>))}*/}
+                </div>
+            </div>}
         </section>
-
     </main>
   )
 }
