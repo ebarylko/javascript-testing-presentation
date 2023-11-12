7ffb92ff472edaebb28a55587c5f155f36301688
EItan Barylko <eitan@barylko.com>
2023-11-12 14:08:11 -0800

    Added test for the rendering of the weekly weather

--- __tests__/homepage.test.js
+++ __tests__/homepage.test.js
@@ -1,5 +1,9 @@
 const {toCelsiusSingleDecimal} = require('../src/app/defaultWeather.js');
 const R = require('ramda');
+import * as React from 'react';
+const { render, screen } =  require('@testing-library/react');
+const { WeeklyWeather } = require('../src/app/defaultWeather.js');
+
 describe("toCelsiusSingleDecimal", () => {
     const fahrenheitTemps = [32, -25, 90, 0, -40];
     describe("When converting from fahrenheit to celsius", () => {
@@ -8,4 +12,16 @@ describe("toCelsiusSingleDecimal", () => {
         expect(actualTemps).toEqual([0, -31.6, 32.2, -17.7, -40]);
     })
     })
+})
+
+describe('WeeklyWeather', () => {
+    const temperatures = [1, 2, 4];
+   describe("When rendering next week's weather", () => {
+       render( <div>
+           <WeeklyWeather weather = {temperatures}/>
+       </div>)
+       it("Should display the correct temperatures", () => {
+           expect(1).toEqual(2);
+       })
+   })
 })
