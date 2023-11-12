a4a8c2cb572c247e8d8794f2704351cf4ba156f2
EItan Barylko <eitan@barylko.com>
2023-11-12 09:54:43 -0800

    Added unit test for celsius conversion function

--- src/app/defaultWeather.js
+++ src/app/defaultWeather.js
@@ -3,7 +3,7 @@ const R = require('ramda');
 const nextWeekTemp = (weather) => R.take(7, weather);
 const toCelsius = (temp) => (temp - 32) * 5 / 9;
 const toSingleDecimal = (temp) => Math.round(temp * 10) / 10;
-const toCelsiusSingleDecimal = R.pipe(toCelsius, toSingleDecimal);
+export const toCelsiusSingleDecimal = R.pipe(toCelsius, toSingleDecimal);
 
 const weatherUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/vancouver"
 
@@ -22,7 +22,6 @@ export function weatherData() {
 }
 
 export function WeeklyWeather(weather) {
-    console.log(weather.weather)
     return weather.weather.map((temp, key) => (
             (<div key={key} className="card">
                     <div className="card-content">
--- sum.test.js
+++ sum.test.js
@@ -1,3 +1,8 @@
+import * as w from "./src/app/defaultWeather.js"
 test('adds 1 + 2 to equal 3', () => {
     expect((1 + 2)).toBe(3);
 });
+
+test("-40 degrees fahrenheit is -40 degrees celsius", () => {
+    expect((w.toCelsiusSingleDecimal(-40))).toBe(-40);
+})
