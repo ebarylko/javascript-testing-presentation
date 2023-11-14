f8598e708cb72c58463eb252d95470926498c2d5
Amir Barylko <amir@barylko.com>
2023-11-13 20:46:02 -0800

    Moved values inside the test

--- __tests__/homepage.test.js
+++ __tests__/homepage.test.js
@@ -1,22 +1,24 @@
 import * as R from 'ramda';
 import * as React from 'react';
-import {cleanup, render, screen} from '@testing-library/react';
+
+import {render, screen} from '@testing-library/react';
+
 
 import { WeeklyWeather } from '@/app/defaultWeather.js';
 import { toCelsiusSingleDecimal } from '@/app/defaultWeather.js';
 
 describe("toCelsiusSingleDecimal", () => {
-    const fahrenheitTemps = [32, -25, 90, 0, -40];
-    const actualTemps = R.map(toCelsiusSingleDecimal, fahrenheitTemps);
     it('Returns the temperature in celsius', () => {
+        const fahrenheitTemps = [32, -25, 90, 0, -40];
+        const actualTemps = R.map(toCelsiusSingleDecimal, fahrenheitTemps);
         expect(actualTemps).toEqual([0, -31.6, 32.2, -17.7, -40]);
     })
 })
 
 const parseTemperatures = (temps) => R.map(
     R.pipe((temp) => temp.textContent, parseInt),
-    temps)
-
+    temps
+)
 
 
 describe('WeeklyWeather', () => {
