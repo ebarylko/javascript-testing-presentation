7f777ec4acfbb2a652b8f0045dd4d9998811f387
Amir Barylko <amir@barylko.com>
2023-11-13 20:39:37 -0800

    Added cypress config with baseUrl

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,9 +1,10 @@
-import defineConfig from "cypress"
+const  { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3008'
   },
 });
--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,9 +1,9 @@
 
-const homeMessage = "Welcome to Weatherme, your friendly neighbourhood weather app."
+const homeMessage = "Welcome to WeatherMe, your friendly neighbourhood weather app."
 
 describe('Homepage', ()=> {
     it( 'should display the welcome message' , () => {
-        cy.visit("localhost:3006")
+        cy.visit("/")
         cy.get('[data-testid = "welcome-message"]').should('have.text', homeMessage)
     })
 })
