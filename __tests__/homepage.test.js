7367ad63922fbcfe6764a0b29152c6e309df2dbe
EItan Barylko <eitan@barylko.com>
2023-11-16 01:15:53 -0800

    Removed weekly-weather-test

--- __tests__/homepage.test.js
+++ __tests__/homepage.test.js
@@ -14,28 +14,3 @@ describe("toCelsiusSingleDecimal", () => {
         expect(actualTemps).toEqual([0, -31.6, 32.2, -17.7, -40]);
     })
 })
-
-const parseTemperatures = (temps) => R.map(
-    R.pipe((temp) => temp.textContent, parseInt),
-    temps
-)
-
-
-describe('WeeklyWeather', () => {
-   describe("When rendering next week's weather", () => {
-       let rerender;
-       beforeEach(() => {
-           rerender = render(<WeeklyWeather weather={[1]}/>).rerender
-       })
-       it.each([
-           [[1, 2]],
-           [[1, 9, -2]],
-           [[-12, -3, 9, 54, 34]]
-       ])('Display the temperatures %s', async (temperatures) => {
-           await rerender(<WeeklyWeather weather={temperatures}/>);
-           const actual = screen.queryAllByTestId('temp');
-           const actualTemps = parseTemperatures(actual);
-           expect(actualTemps).toEqual(temperatures);
-       })
-   })
-})
