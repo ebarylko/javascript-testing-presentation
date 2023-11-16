5da17d04b1b188eb27078311584610019bd32ff3
EItan Barylko <eitan@barylko.com>
2023-11-16 01:13:41 -0800

    Removed weekly-weather-test

--- __tests__/homepage.test.js
+++ __tests__/homepage.test.js
@@ -1,10 +1,4 @@
 import * as R from 'ramda';
-import * as React from 'react';
-
-import {render, screen} from '@testing-library/react';
-
-
-import { WeeklyWeather } from '@/app/defaultWeather.js';
 import { toCelsiusSingleDecimal } from '@/app/defaultWeather.js';
 
 describe("toCelsiusSingleDecimal", () => {
@@ -15,27 +9,3 @@ describe("toCelsiusSingleDecimal", () => {
     })
 })
 
-const parseTemperatures = (temps) => R.map(
-    R.pipe((temp) => temp.textContent, parseInt),
-    temps
-)
-
-
-describe('WeeklyWeather', () => {
-   describe("When rendering next week's weather", () => {
-       let rerender;
-       beforeEach(() => {
-           rerender = render(<WeeklyWeather weather={[1]}/>).rerender
-       })
-       it.each([
-           [[1, 2]],
-           [[1, 9, -2]],
-           [[-12, -3, 9, 54, 34]]
-       ])('Display the temperatures %s', async (temperatures) => {
-           await rerender(<WeeklyWeather weather={temperatures}/>);
-           const actual = screen.queryAllByTestId('temp');
-           const actualTemps = parseTemperatures(actual);
-           expect(actualTemps).toEqual(temperatures);
-       })
-   })
-})
