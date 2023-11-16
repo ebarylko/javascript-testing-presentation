0269dde8ba1b4fcaf9aafec8a2d09d30f3f16d0f
EItan Barylko <eitan@barylko.com>
2023-11-16 01:14:59 -0800

    Removed to-celsius-test

--- __tests__/homepage.test.js
+++ __tests__/homepage.test.js
@@ -1,11 +1,4 @@
 import * as R from 'ramda';
 import { toCelsiusSingleDecimal } from '@/app/defaultWeather.js';
 
-describe("toCelsiusSingleDecimal", () => {
-    it('Returns the temperature in celsius', () => {
-        const fahrenheitTemps = [32, -25, 90, 0, -40];
-        const actualTemps = R.map(toCelsiusSingleDecimal, fahrenheitTemps);
-        expect(actualTemps).toEqual([0, -31.6, 32.2, -17.7, -40]);
-    })
-})
 
